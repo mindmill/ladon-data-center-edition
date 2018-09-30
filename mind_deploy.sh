8f4dad43fa9365b34eb06964b0a1d0359fa34445
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2018-09-30 21:58:52 +0200

    internal deployment file

--- mind_deploy.sh
+++ mind_deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ssh ralf@appserver1 ' fuser -k 8088/tcp '
 scp ./target/ladon_de_2.0.0.jar ralf@appserver1:~/deploy/
-ssh ralf@appserver1 ' nohup java -Dserver.port=8088 -Dlocalsetup -jar ./deploy/ladon_de_2.0.0.jar  >ladon.log &'
+ssh ralf@appserver1 ' nohup java -Dserver.port=8088 -Dlocalsetup=true -jar ./deploy/ladon_de_2.0.0.jar  >ladon.log &'
