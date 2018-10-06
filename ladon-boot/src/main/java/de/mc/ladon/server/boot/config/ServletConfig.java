429f628490723c5721536a326a3185755188df2c
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2018-10-06 20:40:30 +0200

    testing without mvc

--- ladon-boot/src/main/java/de/mc/ladon/server/boot/config/ServletConfig.java
+++ ladon-boot/src/main/java/de/mc/ladon/server/boot/config/ServletConfig.java
@@ -10,19 +10,10 @@ import de.mc.ladon.s3server.servlet.S3Servlet;
 import de.mc.ladon.server.s3.LadonS3Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.HttpServletBean;
-
-import javax.servlet.MultipartConfigElement;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * ServletConfig
@@ -35,20 +26,6 @@ public class ServletConfig {
     @Autowired
     private S3Repository s3r;
 
-    @Bean
-    public ServletRegistrationBean redirectServlet() {
-        ServletRegistrationBean registration = new ServletRegistrationBean(new HttpServletBean() {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                resp.sendRedirect("/admin/overview");
-            }
-        });
-        registration.setName("redirectServlet");
-        registration.addUrlMappings("/");
-        registration.setLoadOnStartup(1);
-        return registration;
-    }
-
 
     @Bean
     public ServletRegistrationBean s3ServletRegistrationBean(LadonS3Config config) {
@@ -59,7 +36,7 @@ public class ServletConfig {
         ServletRegistrationBean registration = new ServletRegistrationBean(
                 s3Servlet);
         registration.setName("s3servlet");
-        registration.addUrlMappings("/services/s3/*");
+        registration.addUrlMappings("/*");
         registration.setLoadOnStartup(1);
         return registration;
     }
@@ -74,28 +51,5 @@ public class ServletConfig {
         return filterBean;
     }
 
-    @Bean
-    FilterRegistrationBean corsFilterRegistrationBean() {
-
-        FilterRegistrationBean filterBean = new FilterRegistrationBean();
-        filterBean.setFilter(new CorsFilter());
-        filterBean.addServletNames("s3servlet");
-        filterBean.setAsyncSupported(true);
-        return filterBean;
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
-    @Bean
-    public ServletRegistrationBean dispatcherServletRegistration(MultipartConfigElement multipartConfigElement) {
-        ServletRegistrationBean registration = new ServletRegistrationBean(
-                dispatcherServlet(), "/admin/*", "/login");
-        registration.setMultipartConfig(multipartConfigElement);
-        registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
-        return registration;
-    }
 
 }
