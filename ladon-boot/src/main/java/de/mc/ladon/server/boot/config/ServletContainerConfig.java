88bc44af92edc92752ab1984c3ccc42003686c37
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2018-10-06 21:02:31 +0200

    spring boot update test

--- ladon-boot/src/main/java/de/mc/ladon/server/boot/config/ServletContainerConfig.java
+++ ladon-boot/src/main/java/de/mc/ladon/server/boot/config/ServletContainerConfig.java
@@ -5,7 +5,7 @@
 package de.mc.ladon.server.boot.config;
 
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
