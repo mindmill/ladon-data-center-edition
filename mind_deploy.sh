cbbda11440bc957e0e27c6ece18ce5e148e58229
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2018-10-15 01:14:21 +0200

    version 2.0.1

--- mind_deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-ssh ralf@appserver1 ' fuser -k 8088/tcp '
-scp ./target/ladon_de_2.*.jar ralf@appserver1:~/deploy/
-ssh ralf@appserver1 ' nohup java -Dserver.port=8088 -Dlocalsetup=true -jar ./deploy/ladon_de_2.*.jar  >ladon.log &'
