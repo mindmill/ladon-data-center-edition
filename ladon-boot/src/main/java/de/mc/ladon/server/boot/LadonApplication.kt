53dfaa246d0880f8ef587a282ddb35952c10d4d1
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2020-01-27 00:05:46 +0100

    ongoing

--- ladon-boot/src/main/java/de/mc/ladon/server/boot/LadonApplication.kt
+++ ladon-boot/src/main/java/de/mc/ladon/server/boot/LadonApplication.kt
@@ -7,8 +7,16 @@ package de.mc.ladon.server.boot
 import de.mc.ladon.server.boot.config.DatabaseConfigImpl
 import de.mc.ladon.server.boot.config.LadonS3ConfigImpl
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.web.servlet.ServletRegistrationBean
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext
+import org.springframework.web.servlet.DispatcherServlet
+
 
 /**
  * LadonApplication
@@ -16,7 +24,29 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  */
 @SpringBootApplication(scanBasePackages = ["de.mc.ladon"])
 @EnableConfigurationProperties(DatabaseConfigImpl::class, LadonS3ConfigImpl::class)
-open class LadonApplication
+open class LadonApplication{
+
+
+    @Bean
+    open fun restApi(parent: ApplicationContext): ServletRegistrationBean {
+        return createChildContextServlet(parent, "/services/rest/*", "rest")
+    }
+
+
+
+
+    private  fun createChildContextServlet(parent: ApplicationContext, path: String, name: String): ServletRegistrationBean {
+        val dispatcherServlet = DispatcherServlet()
+        val ctx = AnnotationConfigWebApplicationContext()
+        ctx.parent = parent
+        ctx.register(PropertyPlaceholderAutoConfiguration::class.java,
+                DispatcherServletAutoConfiguration::class.java)
+        dispatcherServlet.setApplicationContext(ctx)
+        val servletRegistrationBean = ServletRegistrationBean(dispatcherServlet, path)
+        servletRegistrationBean.setName(name)
+        return servletRegistrationBean
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(LadonApplication::class.java, *args)
