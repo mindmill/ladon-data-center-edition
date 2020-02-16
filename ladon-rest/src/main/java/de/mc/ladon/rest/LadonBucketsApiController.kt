475a6a8fc9dd8775e50d3c1c2cc34916f4f83cc7
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2020-02-16 15:30:26 +0100

    ongoing

--- ladon-rest/src/main/java/de/mc/ladon/rest/LadonBucketsApiController.kt
+++ ladon-rest/src/main/java/de/mc/ladon/rest/LadonBucketsApiController.kt
@@ -1,12 +1,16 @@
 package de.mc.ladon.rest
 
+import de.mc.ladon.server.core.api.BucketNotEmpty
+import de.mc.ladon.server.core.api.LadonRepository
 import de.mc.ladon_gen.rest.api.BucketsApi
 import de.mc.ladon_gen.rest.model.Bucket
 import de.mc.ladon_gen.rest.model.ResponseSuccess
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiParam
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
@@ -18,29 +22,36 @@ import javax.validation.Valid
 @Controller
 @RequestMapping("/api/rest/v1")
 @Api(value = "meta", description = "Ladon Buckets API", tags = ["Buckets"])
-open class LadonBucketsApiController(
-
+open class LadonBucketsApiController @Autowired constructor(
+        val ladonRepositoy: LadonRepository
 ) : BucketsApi {
 
     override fun createBucket(
             @ApiParam(value = "", required = true)
             @PathVariable("bucket") bucket: String,
             @ApiParam(value = "Optional description in *Markdown*", required = true)
-            @Valid @RequestBody body: Bucket?): ResponseEntity<Bucket>? { // do some magic!
+            @Valid @RequestBody body: Bucket?): ResponseEntity<Bucket>? {
+        ladonRepositoy.createNewBucket(getUserId(),bucket)
         return ResponseEntity(HttpStatus.OK)
     }
 
 
     override fun deleteBucket(
             @ApiParam(value = "", required = true)
-            @PathVariable("bucket") bucket: String): ResponseEntity<ResponseSuccess?>? { // do some magic!
-        return ResponseEntity(HttpStatus.OK)
+            @PathVariable("bucket") bucket: String): ResponseEntity<ResponseSuccess>? {
+        try {
+            ladonRepositoy.deleteEmptyBucket(getUserId(), bucket)
+        } catch (e: BucketNotEmpty) {
+            return ResponseEntity.ok(ResponseSuccess().success(false).reason(e.message))
+        }
+        return ResponseEntity.ok(ResponseSuccess().success(true))
     }
 
 
     override fun getBucket(
             @ApiParam(value = "", required = true)
-            @PathVariable("bucket") bucket: String): ResponseEntity<Bucket?>? { // do some magic!
+            @PathVariable("bucket") bucket: String): ResponseEntity<Bucket>? {
+
         return ResponseEntity(HttpStatus.OK)
     }
 
@@ -51,9 +62,9 @@ open class LadonBucketsApiController(
             limit: Long?,
             @ApiParam(value = "")
             @RequestParam(value = "page", required = false)
-            page: Long?): ResponseEntity<List<Bucket>>? { // do some magic!
-        return ResponseEntity(HttpStatus.OK)
+            page: Long?): ResponseEntity<List<Bucket>>? {
+        return ResponseEntity.ok(ladonRepositoy.listBuckets(getUserId()).map { Bucket().name(it) })
     }
 
-
+ private fun getUserId() = SecurityContextHolder.getContext().authentication.name
 }
