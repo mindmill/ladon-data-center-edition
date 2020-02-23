b8d367ed9dae03be5b35ea4aa4c3fa9281e40945
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2020-02-23 17:26:25 +0100

    fix free space display

--- ladon-boot/src/main/java/de/mc/ladon/server/boot/controller/pages/SystemPageController.kt
+++ ladon-boot/src/main/java/de/mc/ladon/server/boot/controller/pages/SystemPageController.kt
@@ -2,11 +2,12 @@ package de.mc.ladon.server.boot.controller.pages
 
 import de.mc.ladon.server.boot.controller.FrameController
 import de.mc.ladon.server.boot.controller.humanReadable
+import de.mc.ladon.server.core.util.PathUtils.getLadonHome
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import java.io.File
 import java.lang.management.ManagementFactory
+import java.nio.file.Paths
 import java.util.*
 
 /**
@@ -23,27 +24,27 @@ class SystemPageController : FrameController() {
         val free = runtime.freeMemory()
         val max = runtime.maxMemory()
         val total = runtime.totalMemory()
-        model.put("rtFree", free)
-        model.put("rtFreePercent", Math.round(free * 100.0 / max))
-        model.put("rtFreeRead", free.humanReadable())
-        model.put("rtMax", max)
-        model.put("rtMaxRead", max.humanReadable())
-        model.put("rtTotal", total)
-        model.put("rtTotalRead", total.humanReadable())
-        model.put("rtTotalPercent", Math.round(total * 100.0 / max))
+        model["rtFree"] = free
+        model["rtFreePercent"] = Math.round(free * 100.0 / max)
+        model["rtFreeRead"] = free.humanReadable()
+        model["rtMax"] = max
+        model["rtMaxRead"] = max.humanReadable()
+        model["rtTotal"] = total
+        model["rtTotalRead"] = total.humanReadable()
+        model["rtTotalPercent"] = Math.round(total * 100.0 / max)
 
-        model.put("rtAvailable", (max - total))
-        model.put("rtAvailablePercent", 100 - Math.round(free * 100.0 / max) - Math.round(total * 100.0 / max))
-        model.put("rtAvailableRead", (max - total).humanReadable())
+        model["rtAvailable"] = (max - total)
+        model["rtAvailablePercent"] = 100 - Math.round(free * 100.0 / max) - Math.round(total * 100.0 / max)
+        model["rtAvailableRead"] = (max - total).humanReadable()
 
-        model.put("time", Date().time)
+        model["time"] = Date().time
 
-        model.put("rtProcessors", runtime.availableProcessors())
-        model.put("fsRoots", File.listRoots())
+        model["rtProcessors"] = runtime.availableProcessors()
+        model["fsRoots"] = arrayOf( Paths.get(getLadonHome()).toFile())
 
-        model.put("osName", System.getProperty("os.name"))
-        model.put("osVersion", System.getProperty("os.version"))
-        model.put("osArch", System.getProperty("os.arch"))
+        model["osName"] = System.getProperty("os.name")
+        model["osVersion"] = System.getProperty("os.version")
+        model["osArch"] = System.getProperty("os.arch")
 
         //model.put("threads" , Thread.getAllStackTraces().keySet())
         //model.put("threads" , ManagementFactory.getThreadMXBean())
@@ -56,33 +57,33 @@ class SystemPageController : FrameController() {
         val free = runtime.freeMemory()
         val max = runtime.maxMemory()
         val total = runtime.totalMemory()
-        model.put("rtFree", free)
-        model.put("rtFreePercent", Math.round(free * 100.0 / max))
-        model.put("rtFreeRead", free.humanReadable())
-        model.put("rtMax", max)
-        model.put("rtMaxRead", max.humanReadable())
-        model.put("rtTotal", total)
-        model.put("rtTotalRead", total.humanReadable())
-        model.put("rtTotalPercent", Math.round(total * 100.0 / max))
+        model["rtFree"] = free
+        model["rtFreePercent"] = Math.round(free * 100.0 / max)
+        model["rtFreeRead"] = free.humanReadable()
+        model["rtMax"] = max
+        model["rtMaxRead"] = max.humanReadable()
+        model["rtTotal"] = total
+        model["rtTotalRead"] = total.humanReadable()
+        model["rtTotalPercent"] = Math.round(total * 100.0 / max)
 
-        model.put("rtAvailable", (max - total))
-        model.put("rtAvailablePercent", 100 - Math.round(free * 100.0 / max) - Math.round(total * 100.0 / max))
-        model.put("rtAvailableRead", (max - total).humanReadable())
+        model["rtAvailable"] = (max - total)
+        model["rtAvailablePercent"] = 100 - Math.round(free * 100.0 / max) - Math.round(total * 100.0 / max)
+        model["rtAvailableRead"] = (max - total).humanReadable()
 
-        model.put("rtProcessors", runtime.availableProcessors())
-        model.put("fsRoots", File.listRoots())
-        model.put("time", Date().time)
+        model["rtProcessors"] = runtime.availableProcessors()
+        model["fsRoots"] = arrayOf( Paths.get(getLadonHome()).toFile())
+        model["time"] = Date().time
 
         try {
             javaClass.classLoader.loadClass("com.sun.management.OperatingSystemMXBean")
             val osBean = ManagementFactory.getPlatformMXBean(com.sun.management.OperatingSystemMXBean::class.java)
-            model.put("jvmLoad", osBean.processCpuLoad * 100)
-            model.put("systemLoad", osBean.systemCpuLoad * 100)
+            model["jvmLoad"] = osBean.processCpuLoad * 100
+            model["systemLoad"] = osBean.systemCpuLoad * 100
         } catch (e: Exception) {
 
             val osBean = ManagementFactory.getPlatformMXBean(java.lang.management.OperatingSystemMXBean::class.java)
-            model.put("jvmLoad", 0)
-            model.put("systemLoad", osBean.systemLoadAverage * 100)
+            model["jvmLoad"] = 0
+            model["systemLoad"] = osBean.systemLoadAverage * 100
 
         }
         return "cpuload"
