d1a3459b752319b1055a7ffb5fe359dacc251562
Ralf Ulrich <ralf.ulrich@mind-consulting.de>
2020-03-01 20:17:44 +0100

    disabled mediatype stuff

--- ladon-boot/src/main/java/de/mc/ladon/server/boot/config/WebConfig.java
+++ ladon-boot/src/main/java/de/mc/ladon/server/boot/config/WebConfig.java
@@ -1,7 +1,9 @@
 package de.mc.ladon.server.boot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -15,5 +17,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new CallContextResolver());
+    }
+
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false).
+                favorParameter(true).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("xml", MediaType.APPLICATION_XML);
     }
 }
